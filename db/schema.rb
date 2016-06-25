31b81307e69051276b52e883f9593116c2f5d9cf
Leonel Badi <lbadi@itba.edu.ar>
2016-06-25 19:19:24 -0300

    add locale to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625200739) do
+ActiveRecord::Schema.define(version: 20160625214724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160625200739) do
     t.float    "lat"
     t.float    "lng"
     t.string   "address"
+    t.string   "locale"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
