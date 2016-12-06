b87be6fbc18887efbf26bcf40475d5033bf41c8f
NicoBuch <nicolas.buchhalter@wolox.com.ar>
2016-12-05 22:51:06 -0300

    added notification type donation deactivated

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,8 @@ class Notification < ActiveRecord::Base
   belongs_to :from, class_name: 'User'
   belongs_to :to, class_name: 'User'
 
-  enum n_type: [:donation_activated, :donation_expired, :activation_time_passed]
+  enum n_type: [:donation_activated, :donation_expired, :activation_time_passed,
+                :donation_deactivated]
 
   validates :to, presence: true
 end
